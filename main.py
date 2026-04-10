e12b2679b6b876d069333ed015138852dd46686f
odinarvhage <odin.arvhage@outlook.com>
2026-04-10 14:42:09 +0200

    Added empty functions for all four parts of CRUD

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ import mysql.connector
 #Change the host to the IP address when we use a remote server.
 connection = mysql.connector.connect(host="localhost",user="root", password="",database="test")
 
+#Function for the Create part of CRUD
+def create_entry():
+    pass
+
+#Function for the Read part of CRUD
+def read_entry():
+    pass
+
+#Function for the Update part of CRUD
+def update_entry():
+    pass
+
+#Function for the Delete part of CRUD
+def delete_entry():
+    pass
+
 #Closes the connection once the program exits.
 connection.close()
 
