4315aa7b1941ec2b06dc537d45663fe6a4ea2c81
odinarvhage <odin.arvhage@outlook.com>
2026-04-10 14:41:55 +0200

    Added connection to mysql

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import mysql.connector
 
-st.write("Hello world")
+#Change the host to the IP address when we use a remote server.
+connection = mysql.connector.connect(host="localhost",user="root", password="",database="test")
+
+#Closes the connection once the program exits.
+connection.close()
+
