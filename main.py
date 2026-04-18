38aa4c6b30194457e2a01af1ea177a640f64d24b
odinarvhage <odin.arvhage@outlook.com>
2026-04-18 17:28:20 +0200

    Added proper loading of environment variables

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from asyncio import all_tasks
+import os
 
 import streamlit as st
 import mysql.connector
-import mysql.connector
-
+from dotenv import load_dotenv
 
 import tables
 
+load_dotenv()
+
+ip = os.getenv("DB_IP")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASS")
+database = os.getenv("DB_NAME")
+
+
 connection = mysql.connector.connect(
-    host="10.212.175.85",
-    user="teamplayer",
-    password="hawktuah1",
-    database="playground")
+    host=ip,
+    user=user,
+    password=password,
+    database=database)
 
 st.set_page_config(
     page_title="Fitness Tracking App",
@@ -36,18 +43,17 @@ def table_exists(table_name, conn):
 
 if not connection.is_connected:
     print("Error connecting to MySQL database.")
+    print("Please check your credentials and you are connected to the NTNU vpn and try again.")
     quit(0)
 
-def init():
-    all_tables = tables.get_tables()
-    for tableArray in all_tables:
-        for table in tableArray:
-            if table_exists(table, connection):
-                print("Table {} exists.".format(table))
-            else:
-                print("Table {} does not exist.".format(table))
-                print("Creating table {}.".format(table))
 
+all_tables = tables.get_tables()
+for tableArray in all_tables:
+    for table in tableArray:
+        if table_exists(table, connection):
+            print("Table {} exists.".format(table))
+        else:
+            print("Table {} does not exist.".format(table))
 
 
 healthButton = st.button("Health")
@@ -56,4 +62,3 @@ userButton = st.button("User")
 
 def initialize_webclient():
     pass
-
